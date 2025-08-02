7b0fbff89ef8691443a0abab85e4f448d0adecb4
MarguJ <margulesjoa@gmail.com>
2025-08-02 00:57:22 -0300

    No c hago lo q puedo

---
+++ Assets/Scripts/MountainGenerator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MountainGenerator : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        VerticesMovement();
+    }
+
+    private void VerticesMovement()
+    {
+        
+    }
+}
