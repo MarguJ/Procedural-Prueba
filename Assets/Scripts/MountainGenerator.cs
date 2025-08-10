0ec7d4788e1edd6eb4720327a1241adfd963cd3f
MarguJ <margulesjoa@gmail.com>
2025-08-09 21:30:00 -0300

    Stop mountain

--- Assets/Scripts/MountainGenerator.cs
+++ Assets/Scripts/MountainGenerator.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class MountainGenerator : MonoBehaviour
 {
-    [SerializeField] protected MeshFilter _filter;
-    [SerializeField][Range(0f, 10f)] protected float _scale = 0.5f;
-    [SerializeField][Range(0f, 100f)] protected float _height = 0.5f;
+    [SerializeField] protected MeshFilter filter;
+    [SerializeField][Range(0f, 10f)] protected float scale = 0.5f;
+    [SerializeField][Range(0f, 100f)] protected float height = 0.5f;
 
     private void Awake()
     {
@@ -13,25 +13,27 @@ public class MountainGenerator : MonoBehaviour
 
     private void GenerateMountain()
     {
-        Mesh mesh = _filter.mesh;
+        Mesh mesh = filter.mesh;
         Vector3[] vertices = mesh.vertices;
         for (int i = 0; i < vertices.Length; i++)
         {
             Vector2 vertex = vertices[i];
-            vertex.y = GetHeight(vertex.x, vertex.y);
+            vertex.y = GetHeight(vertex.y);
             vertices[i] = vertex;
         }
         mesh.vertices = vertices;
         mesh.RecalculateNormals();
     }
 
-    private float GetHeight(float x, float y)
+    private float GetHeight(float y)
     {
+        float uai = y;
         float angleDeg = UnityEngine.Random.Range(67.5f, 112.5f);
         
         float angleRad = angleDeg * Mathf.Deg2Rad;
-        x = y * Mathf.Cos(angleRad) / Mathf.Sin(angleRad);
-        return (x,y);
+        float x = uai * Mathf.Cos(angleRad) / Mathf.Sin(angleRad);
+        
+        return x;
     }
 
     private void OnDrawGizmos()
