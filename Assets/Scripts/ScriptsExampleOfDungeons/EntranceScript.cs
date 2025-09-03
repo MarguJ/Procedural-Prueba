3bb714a843e4d9700f5e46ce964b87edf1e014fb
MarguJ <margulesjoa@gmail.com>
2025-09-03 00:11:49 -0300

    Interseccion creada (no entera)

--- Assets/Scripts/ScriptsExampleOfDungeons/EntranceScript.cs
+++ Assets/Scripts/ScriptsExampleOfDungeons/EntranceScript.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using Object = System.Object;
 using Random = UnityEngine.Random;
 
 namespace ScriptsExampleOfDungeons
@@ -10,11 +6,10 @@ namespace ScriptsExampleOfDungeons
     public class  EntranceScript : MonoBehaviour //para el posta cambiarle el nombre a SpawnScript
     {
         public GameObject doorGameObject;
-        public GameObject childExit;
         public GameObject uniqueHall;
         public ExitScript exitScript;
         public GameObject childHall;
-        private int maxOfRoomSpawn = 8;
+        private int maxOfRoomSpawn = 9;
         private int minOfRoomSpawn = 5;
         private int quantityOfRooms;
         private int roomHallwayIntersection;
@@ -66,8 +61,22 @@ namespace ScriptsExampleOfDungeons
             {
                 //registrar a todos los hallways ya creados
                 ToSetExitNumber();
-                //por cada hallway (For)
-                //Que haya una posibilidad de que un room o un hallway aparezca si aparece un room sumar +1 a rooms
+                //por cada hallway (ForEach)
+                roomHallwayIntersection = Random.Range(1,8);
+                if (roomHallwayIntersection == 1 || roomHallwayIntersection == 2 || roomHallwayIntersection == 3 || roomHallwayIntersection == 4) //Hallway
+                {
+                    Debug.Log("Hallway Generated");
+                }
+                else if (roomHallwayIntersection == 5 || roomHallwayIntersection == 6) //Intersection
+                {
+                    Debug.Log("Intersection Generated");
+                }
+                else if (roomHallwayIntersection == 7) //Room
+                {
+                    Debug.Log("Room Generated");
+                    rooms++;
+                }
+                //Que haya una posibilidad de que un room o un hallway o una intersection aparezca, si aparece un room sumar +1 a rooms
             }
         }
 
